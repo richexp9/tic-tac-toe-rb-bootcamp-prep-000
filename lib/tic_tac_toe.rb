15ee244e639a48c39bda27e67c537164d222094e
Richard Loera <richexp9@gmx.com>
2018-01-02 11:56:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,14 @@
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]  # M,iddle row
-  # ETC, an array for each win combination
-]
+    WIN_COMBINATIONS = [
+      [0,1,2], # Top row
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]  # M,iddle row
+      # ETC, an array for each win combination
+    ]
 
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} \n"
